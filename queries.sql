03549c38c6f8fc48cb583d965b6962252ab4125f
Ling Tang <159871326+ling-tang0922@users.noreply.github.com>
2025-04-14 23:03:07 -0700

    updated query 1

--- queries.sql
+++ queries.sql
@@ -1,5 +1,6 @@
 -- Query #1
--- Selecting the Number of Trips Associated from Each Route, sorting from the route with the most amount of trips to the least amount
+-- Selecting the Number of Trips Associated from Each Route
+-- Sorting from the route with the most amount of trips to the least, take only top 20 
 SELECT
   r.route_short_name,
   r.route_long_name,
@@ -11,7 +12,7 @@ JOIN `bigquery-public-data.san_francisco_transit_muni.routes` r
 ON t.route_id = r.route_id
 GROUP BY r.route_short_name, r.route_long_name
 ORDER BY num_trips DESC
-LIMIT 100;
+LIMIT 20;
 
 -- Query #2
 
